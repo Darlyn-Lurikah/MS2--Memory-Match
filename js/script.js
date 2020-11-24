e7edc2dbcaff7b58e91cff5e0d9581f5c846a022
Darlyn Norlay <norlaydee@hotmail.co.uk>
2020-11-24 22:21:18 +0000

    Cards turning 180 degrees

---
+++ js/script.js
@@ -0,0 +1,18 @@
+
+//To turn the cards on click//
+$(document).ready(function() {
+
+    $('.game-card').click(function(){
+        $(this).toggleClass('turn');
+        
+    });
+    
+});
+
+
+
+
+
+
+
+
